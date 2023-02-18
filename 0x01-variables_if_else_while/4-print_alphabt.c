d26bbf2d2ae61be7ab7eb66874d32be08e9acb78
Cestoragu <josephmaduabuchiagu@gmail.com>
2023-02-17 20:01:48 -0800

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - returns alphabets both except e and q
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
-
 {
-	char ch = 'a';
-	while (ch <= 'z')
-	{
-		if ((ch != 'e') && (ch != 'q'))
-		{
-			putchar(ch);
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
 		}
-		ch++;
-	}
 	putchar('\n');
 	return (0);
 }
