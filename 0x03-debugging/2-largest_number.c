59f28a20be2dfdbeb540050581ec21dd902a1ae4
Cestoragu <josephmaduabuchiagu@gmail.com>
2023-02-23 10:35:17 +0100

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,22 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-	if (b > C)
 		largest = a;
-	else if (a > c)
-		largest = a;
-	else
+	}
+	else if (b > c && b > c)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
 		largest = c;
 	}
-	esle
+	else
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
+		largest = b;
 	}
+
 	return (largest);
 }
