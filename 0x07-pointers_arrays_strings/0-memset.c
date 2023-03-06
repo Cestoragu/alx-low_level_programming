e00e88e0ab62ca6e4cc2d4ffe03611de77f2c389
Cestoragu <josephmaduabuchiagu@gmail.com>
2023-03-06 19:11:15 +0100

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - entry point
+ * _memset - Entry point
  * @s: pointed destination
  * @b: constant byte
  * @n: bytes
- * Return: always 0 (success)
+ * Return: Always 0 (success)
  */
 
 char *_memset(char *s, char b, unsigned int n)
