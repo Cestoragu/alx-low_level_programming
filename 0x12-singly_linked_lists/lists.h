ffde63bd3475635acea314f3f9a829934d153120
Cestoragu <josephmaduabuchiagu@gmail.com>
2023-03-30 11:42:40 +0100

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,24 +1,21 @@
-#ifndef LINKED
-#define LINKED
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * struck list_s - singly linked list
+ * struck list_t - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 
-typedef struct list_s
+typedef struct list_t
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next;
-} list_s;
+	struct list_t *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
