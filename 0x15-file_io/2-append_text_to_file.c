175aef8fa1161ddd64e49e77e662fdce180fef36
Cestoragu <josephmaduabuchiagu@gmail.com>
2023-04-09 12:37:35 +0100

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 	p = open(filename, O_WRONLY | O_APPEND);
-	l = write(o, text_content, len);
+	l = write(p, text_content, len);
 	if (p == -1 || l == -1)
 		return (-1);
 	close(p);
